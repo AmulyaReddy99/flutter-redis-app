52a7478ca4abe82b14bfcec821a47b87c9e2ef9e
Amulya Reddy Konda <amulyareddyk@yahoo.in>
2021-01-20 07:57:27 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redis_app/pages/main-page.dart';
+import 'package:redis/redis.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(FlutterRedisApp());
 }
 
-class FlutterRedisApp extends StatelessWidget {
+class FlutterRedisApp extends StatefulWidget {
+  _FlutterRedisApp createState() => _FlutterRedisApp();
+  // FlutterRedisApp() {
+  //   RedisConnection conn = new RedisConnection();
+  //   conn.connect('localhost',6379);
+  // }
+}
+
+class _FlutterRedisApp extends State<FlutterRedisApp> {
+  bool _initialized = false;
+  bool _error = false;
+
+  void initializeFlutterFire() async {
+    try {
+      await Firebase.initializeApp();
+        setState(() {
+          _initialized = true;
+        });
+      } catch(e) {
+        setState(() {
+          _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    if(_error) {
+      print("ERROR in connecting to database");
+    }
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Redis Page',
